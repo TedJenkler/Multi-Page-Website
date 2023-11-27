8931b71c3b54b8c602df229f8d6bef0994ed874d
TedJenkler <Tedjenkler@gmail.com>
2023-11-27 01:34:46 -0200

    keyword card

--- src/Components.js/HomePage/HomePage.js
+++ src/Components.js/HomePage/HomePage.js
@@ -1,4 +1,8 @@
 import phoneframe from "../../assets/home/desktop/image-hero-phone.png"
+import passionate from "../../assets/home/desktop/illustration-passionate.svg"
+import resourceful from "../../assets/home/desktop/illustration-resourceful.svg"
+import friendly from "../../assets/home/desktop/illustration-friendly.svg"
+
 
 const HomePage = () => {
     return (
@@ -31,7 +35,21 @@ const HomePage = () => {
             </div>
             <div className="row">
                 <div className="keyword-area">
-
+                    <div className="passionate-card">
+                        <img src={passionate} alt="passionate" />
+                        <h2>Passionate</h2>
+                        <p>Each project starts with an in-depth brand research to ensure we only create products that serve a purpose. We merge art, design, and technology into exciting new solutions.</p>
+                    </div>
+                    <div className="resourceful-card">
+                        <img src={resourceful} alt="resourceful" />
+                        <h2>Resourceful</h2>
+                        <p>Everything that we do has a strategic purpose. We use an agile approach in all of our projects and value customer collaboration. It guarantees superior results that fulfill our clients’ needs.</p>
+                    </div>
+                    <div className="friendly-card">
+                        <img src={friendly} alt="friendly" />
+                        <h2>Friendly</h2>
+                        <p>We are a group of enthusiastic folks who know how to put people first. Our success depends on our customers, and we strive to give them the best experience a company can provide.</p>
+                    </div>
                 </div>
             </div>
         </div>
