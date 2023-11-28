ea57b1170acf585cb3f32a4a57b6379a76007cbb
TedJenkler <Tedjenkler@gmail.com>
2023-11-28 10:47:49 -0200

    checkpoint contactpage

--- src/Components.js/Pages/Contact.js
+++ src/Components.js/Pages/Contact.js
@@ -1,6 +1,22 @@
-const Contact = () => {
+import CapitalLocations from "../CapitalLocations"
+
+const Contact = ( {hamburger} ) => {
     return (
-        <></>
+        <div className={hamburger === false ? "row" : "row blurry"}>
+            <div className="hero hero-contact">
+                <form className="contact">
+                    <h1>Contact Us</h1>
+                    <p>Ready to take it to the next level? Let’s talk about your project or idea and find out how we can help your business grow. If you are looking for unique digital experiences that’s relatable to your users, drop us a line.</p>
+                    <div className="inputarea">
+                        <input type="name"></input>
+                        <input type="email"></input>
+                        <input type="phone"></input>
+                        <input type="textarea"></input>
+                    </div>
+                </form>
+            </div>
+            <CapitalLocations />
+        </div>
     )
 }
 
