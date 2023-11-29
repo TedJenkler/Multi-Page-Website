0a2dfa7ac3e71f6dc92a1c3f7c67aaada88f7c34
TedJenkler <Tedjenkler@gmail.com>
2023-11-28 23:53:11 -0200

    contact page done - logic left

--- src/Components.js/Pages/Contact.js
+++ src/Components.js/Pages/Contact.js
@@ -4,14 +4,15 @@ const Contact = ( {hamburger} ) => {
     return (
         <div className={hamburger === false ? "row" : "row blurry"}>
             <div className="hero hero-contact">
-                <form className="contact">
+                <form className="contact" onSubmit={(e) => {e.preventDefault()}}>
                     <h1>Contact Us</h1>
                     <p>Ready to take it to the next level? Let’s talk about your project or idea and find out how we can help your business grow. If you are looking for unique digital experiences that’s relatable to your users, drop us a line.</p>
                     <div className="inputarea">
-                        <input type="name"></input>
-                        <input type="email"></input>
-                        <input type="phone"></input>
-                        <input type="textarea"></input>
+                        <input id="name" type="name" placeholder="Name"></input>
+                        <input id="email" type="email" placeholder="Email Address"></input>
+                        <input id="phone" type="phone" placeholder="Phone"></input>
+                        <input id="textarea" type="textarea" placeholder="Your Message"></input>
+                        <button id="submit" type="submit" >SUBMIT</button>
                     </div>
                 </form>
             </div>
