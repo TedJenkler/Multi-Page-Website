7fda4ff1af347d3fadbad1d0e2c401c1ecdf4dd4
TedJenkler <Tedjenkler@gmail.com>
2023-11-30 08:33:35 -0200

    responsive for mobile, tablet done

--- src/Components.js/ContentItem.js
+++ src/Components.js/ContentItem.js
@@ -3,8 +3,10 @@ const ContentItem = ({ img, h2, p }) => {
     return (
         <div className="content-item">
             <img src={img} alt={h2} />
-            <h2>{h2}</h2>
-            <p>{p}</p>
+            <div className="responsive-content">
+                <h2>{h2}</h2>
+                <p>{p}</p>
+            </div>
         </div>
     )
 }
