5f33f855028f6678d230f664736b704167c95acf
TedJenkler <Tedjenkler@gmail.com>
2023-12-04 02:14:58 -0200

    About Page done mobile, tablet and desktop

--- src/Components.js/CapitalLocations.js
+++ src/Components.js/CapitalLocations.js
@@ -6,15 +6,21 @@ const CapitalLocations = () => {
     return (
         <>
             <div className="capitallocation-area">
-                <div id="canada"><img id="canadarotateback" src={canada} alt="canada" /></div>
-                <h2>CANADA</h2>
-                <button className="btn">SEE LOCATION</button>
-                <img src={australia} alt="australia" />
-                <h2>AUSTRALIA</h2>
-                <button className="btn">SEE LOCATION</button>
-                <img src={unitedkingdom} alt="unitedkingdom" />
-                <h2>UNITED KINGDOM</h2>
-                <button className="btn">SEE LOCATION</button>
+                <div className="capitallocation-item">
+                    <div id="canada"><img id="canadarotateback" src={canada} alt="canada" /></div>
+                    <h2 id="canadah2">CANADA</h2>
+                    <button className="btn">SEE LOCATION</button>
+                </div>
+                <div className="capitallocation-item">
+                    <img src={australia} alt="australia" />
+                    <h2>AUSTRALIA</h2>
+                    <button className="btn">SEE LOCATION</button>
+                </div>
+                <div className="capitallocation-item">
+                    <img src={unitedkingdom} alt="unitedkingdom" />
+                    <h2>UNITED KINGDOM</h2>
+                    <button className="btn">SEE LOCATION</button>
+                </div>
             </div>
         </>
     )
